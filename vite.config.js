d4f167499bddee6769e0d540aac6a666a6dda46c
Dmytro Karpenko <kardmitriy@gmail.com>
2025-04-30 13:03:30 +0300

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Vite-Template/', // Замініть на назву вашого репозиторію
+  base: '/', // Замініть на назву вашого репозиторію
 });
