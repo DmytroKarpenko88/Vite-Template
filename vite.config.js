dd25845ecc7ad333997080d19cf49a4ec578914b
Dmytro Karpenko <kardmitriy@gmail.com>
2025-05-01 12:49:10 +0300

    gh-page

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Замініть на назву вашого репозиторію
+  base: '/Vite-Template/', // Замініть на назву вашого репозиторію
 });
