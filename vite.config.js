2ca57ade684f3bae7afc5ce3602fb74dba82d6b1
Dmytro Karpenko <kardmitriy@gmail.com>
2025-07-01 14:21:02 +0300

    Update tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/Vite-Template/', // Замініть на назву вашого репозиторію
 });
